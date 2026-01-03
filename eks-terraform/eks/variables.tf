a5e13c7a53bc8081d93a6348d92f934a3fe909c3
mahmoudsallem <salem7ouda@gmail.com>
2026-01-03 14:13:06 +0200

    feat: Initialize EKS cluster and VPC infrastructure with Terraform state and providers.

--- eks-terraform/eks/variables.tf
+++ eks-terraform/eks/variables.tf
@@ -1,11 +1,24 @@
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster.arn
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
 
-  vpc_config {
-    subnet_ids              = concat(var.public_subnets, var.private_subnets)
-    endpoint_private_access = true
-    endpoint_public_access  = true
-    public_access_cidrs     = [var.allowed_public_ip]
-  }
+variable "vpc_id" {
+  description = "VPC ID where EKS cluster will be deployed"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "allowed_public_ip" {
+  description = "CIDR block allowed to access the EKS cluster API"
+  type        = string
 }
