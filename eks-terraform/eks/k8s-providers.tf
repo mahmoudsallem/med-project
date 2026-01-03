3071652e447c016f6755a4f8186f9c91276e7244
kasssas <elkassasmostafa29@gmail.com>
2026-01-03 14:01:19 +0200

    fix vars error eks-terraform for dev branch

--- eks-terraform/eks/k8s-providers.tf
+++ eks-terraform/eks/k8s-providers.tf
@@ -1,13 +1,13 @@
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
